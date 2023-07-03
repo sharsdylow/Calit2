87bc1d6f8e661e0d5ccf972b5d7e300f521c6323
Eve Zhang <xuyinz1@uci.edu>
2023-07-03 16:09:10 -0700

    complete Navbar and pages routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import Navbar from "./components/Navbar";
+import Sensor from "./pages/Sensor";
+import Camera from "./pages/Camera";
+import Setting from "./pages/Setting";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <Router>
+      <div >
+        <Navbar />
+        <Switch>
+          <Route path="/sensor" component={Sensor} />
+          <Route path="/camera" component={Camera} />
+          <Route path="/setting" component={Setting} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Sensor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sensor() {
+  return (
+    <div>
+        <h1 >Sensors</h1>
+    </div>
+  )
+}
+
+export default Sensor
