d9a2435186c54733294144826107f2fd2fc32d77
Eve Zhang <xuyinz1@uci.edu>
2023-07-05 13:54:40 -0700

    complete origin realtime line chart component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import RealtimeSensorChart from '../components/RealtimeSensorChart'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h3>Sensor 1</h3>
+      <RealtimeSensorChart />
+    </div>
+  );
 }
