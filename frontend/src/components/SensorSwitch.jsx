33cb6529feb7199c3002485fef5356a4da921e23
Eve Zhang <xuyinz1@uci.edu>
2023-07-12 10:57:05 -0700

    add sensor switches component

---
+++ frontend/src/components/SensorSwitch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+export default function SensorSwitch({group: {id, sensors}}) {
+  const switches = sensors.map(sensor =>
+    <FormControlLabel control={<Switch defaultChecked />} label={sensor} />
+  )
+  return (
+    <div>
+      <FormGroup>
+        {switches}
+      </FormGroup>
+    </div>
+  )
+}
